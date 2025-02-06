8f3b0ba3da9558b23c15894526e9cafe99d3524b
Tomas Votruba <tomas.vot@gmail.com>
2025-02-06 12:36:19 +0100

    fix

--- composer-dependency-analyser.php
+++ composer-dependency-analyser.php
@@ -3,10 +3,5 @@
 declare(strict_types=1);
 
 use ShipMonk\ComposerDependencyAnalyser\Config\Configuration;
-use ShipMonk\ComposerDependencyAnalyser\Config\ErrorType;
 
-return (new Configuration())
-    ->ignoreErrorsOnPackage('phpunit/phpunit', [ErrorType::DEV_DEPENDENCY_IN_PROD])
-    // optional dependency for symfony config generator command
-    ->ignoreErrorsOnPackage('symfony/config', [ErrorType::DEV_DEPENDENCY_IN_PROD])
-    ->ignoreErrorsOnPackage('symfony/dependency-injection', [ErrorType::DEV_DEPENDENCY_IN_PROD]);
+return (new Configuration());
